8570b5e709104f10c1216128e98f83f3e60a37d9
Grzegorz Kryszczuk <gkryszcz@c3r11s4.42warsaw.pl>
2025-03-28 13:08:52 +0100

    first commit

--- get_next_line.h
+++ get_next_line.h
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkryszcz <gkryszcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/12 10:51:32 by gkryszcz          #+#    #+#             */
-/*   Updated: 2025/03/18 16:41:23 by gkryszcz         ###   ########.fr       */
+/*   Created: 2025/03/18 15:42:05 by gkryszcz          #+#    #+#             */
+/*   Updated: 2025/03/18 15:45:42 by gkryszcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include "get_next_line.h"
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <sys/types.h>
 
-int main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("tekst", O_RDONLY);
-	if (fd < 0)
-		return (1);
-	line = get_next_line(fd);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
-}
+#endif
